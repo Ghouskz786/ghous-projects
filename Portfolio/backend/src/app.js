66f854973e47505f980f39d0e073e88a919a03b7
Ghouskz786 <ghous.kz.786@gmail.com>
2026-03-27 22:33:23 +0500

    Fix CORS origin URL by removing trailing slash

--- Portfolio/backend/src/app.js
+++ Portfolio/backend/src/app.js
@@ -9,7 +9,7 @@ const contactRoute = require("./Routes/contact.js");
 const resumeRoute = require("./Routes/resume.js");
 require("dotenv").config();
 const app = express();
-app.use(cors({ credentials: true, origin: "https://ghous-projects-tu3t.vercel.app/" }));
+app.use(cors({ credentials: true, origin: "https://ghous-projects-tu3t.vercel.app" }));
 let isConnected = false;
 app.use(async (req, res, next) => {
   if (!isConnected) {
