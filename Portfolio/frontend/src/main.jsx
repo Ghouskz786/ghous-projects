84b83a8517861ffb1a3906fa3f1dbbece0019fca
Ghouskz786 <ghous.kz.786@gmail.com>
2026-03-28 18:37:38 +0500

    Add data fetching and loader to main component

--- Portfolio/frontend/src/main.jsx
+++ Portfolio/frontend/src/main.jsx
@@ -2,12 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import UpdateElement from "./components/UpdateElement.jsx";
-import App from "./App.jsx";
+import App, { HydrateFallback } from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddProjectElement from "./components/AddProjectElement.jsx";
-
+const handleLoader = async () => {
+  const res = await (
+    await fetch("http://localhost:3000/project/get-project", {
+      method: "get",
+      credentials: "include",
+    })
+  ).json();
+  if (res.success) {
+    return { success: true, projects: res.projects };
+  } else {
+    return { success: false, Errors: res.messages };
+  }
 const route = createBrowserRouter([
-  { path: "/", element: <App></App> },
+  { path: "/", element: <App></App>,    loader: handleLoader,
+    hydrateFallbackElement: <HydrateFallback /> },
   { path: "/update-portfolio", element: <UpdateElement /> },
   { path: "/update-portfolio/add-project", element: <AddProjectElement /> },
 ]);
@@ -16,6 +28,7 @@ createRoot(document.getElementById("root")).render(
     <StrictMode>
       <App />
     </StrictMode>
-    ,
   </RouterProvider>,
 );
+
+};
